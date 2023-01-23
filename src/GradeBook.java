e2b35ce0c9ceb5548398a204b5c63b9d60184396
Nikolas Furuta <nikolasfuruta@gmail.com>
2023-01-23 19:28:30 -0300

    Create GradeBook class

---
+++ src/GradeBook.java
@@ -0,0 +1,15 @@
+public class GradeBook {
+
+    public String nome;
+    public int qtd;
+    public float[] notas;
+
+    GradeBook(String nome, int qtd, float[] notas){
+        this.nome = nome;
+        this.qtd = qtd;
+        this.notas = notas;
+    }
+
+
+}
+
