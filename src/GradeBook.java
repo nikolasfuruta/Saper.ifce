8f7cac2befa0ed7c5655f1236eaaa7b7117fc577
Nikolas Furuta <nikolasfuruta@gmail.com>
2023-01-23 21:11:16 -0300

    finish class exercise

--- src/GradeBook.java
+++ src/GradeBook.java
@@ -1,13 +1,120 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class GradeBook {
+    /*Vamos desenvolver uma classe GradeBook que professores
+    podem usar para armazenar as notas de uma prova dos alunos e
+    mostrar um relatório que contém as notas, média das notas,
+    menor nota, maior nota, e uma distribuição de notas. A classe
+    GradeBook deve ter como atributos o nome do curso e as notas
+    dos alunos do curso. A distribuição de notas deve levar em
+    consideração a frequência de notas em cada intervalo a seguir:
+    [0.0, 1.0), [1.0, 2.0), [2.0, 3.0), [3.0, 4.0), [4.0, 5.0),
+    [5.0, 6.0), [6.0, 7.0), [7.0, 8.0), [8.0, 9.0), [9.0, 10.0]*/
 
     public String nome;
     public int qtd;
+    public float soma = 0f;
     public float[] notas;
+    private double media;
+    private float max;
+    private float min;
+    private final HashMap<String,Integer> frequencia = new HashMap<>();
 
-    GradeBook(String nome, int qtd, float[] notas){
+    GradeBook(String nome, int qtd){
         this.nome = nome;
         this.qtd = qtd;
-        this.notas = notas;
+        this.notas = new float[this.qtd];
+    }
+
+    public void addNotas(float nota, int pos){
+        if(pos<this.qtd) {
+            this.notas[pos] = nota;
+            soma += nota;
+        }
+        else System.out.println("Out of Bound!");
+    }
+
+    public void calcular(){
+        this.media();
+        this.getMaiorMenor();
+        this.getDistribution();
+    }
+
+    private void media(){
+        media = (double) soma/this.qtd;
+    }
+
+    private void getMaiorMenor(){
+        Arrays.sort(this.notas);
+        max = this.notas[this.notas.length-1];
+        min = this.notas[0];
+    }
+
+    private void getDistribution(){
+        for(float nota : this.notas){
+            if(nota >=0.0 && nota<1.0){
+                String string = "[0.0, 1.0)";
+                if(frequencia.containsKey(string)) frequencia.put(string,frequencia.get(string)+1);
+                else frequencia.put(string,1);
+            }
+            else if(nota >=1.0 && nota<2.0){
+                String string = "[1.0, 2.0)";
+                if(frequencia.containsKey(string)) frequencia.put(string,frequencia.get(string)+1);
+                else frequencia.put(string,1);
+            }
+            else if(nota >=2.0 && nota<3.0){
+                String string = "[2.0, 3.0)";
+                if(frequencia.containsKey(string)) frequencia.put(string,frequencia.get(string)+1);
+                else frequencia.put(string,1);
+            }
+            else if(nota >=3.0 && nota<4.0){
+                String string = "[3.0, 4.0)";
+                if(frequencia.containsKey(string)) frequencia.put(string,frequencia.get(string)+1);
+                else frequencia.put(string,1);
+            }
+            else if(nota >=4.0 && nota<5.0){
+                String string = "[4.0, 5.0)";
+                if(frequencia.containsKey(string)) frequencia.put(string,frequencia.get(string)+1);
+                else frequencia.put(string,1);
+            }
+            else if(nota >=5.0 && nota<6.0){
+                String string = "[5.0, 6.0)";
+                if(frequencia.containsKey(string)) frequencia.put(string,frequencia.get(string)+1);
+                else frequencia.put(string,1);
+            }
+            else if(nota >=7.0 && nota<8.0){
+                String string = "[7.0, 8.0)";
+                if(frequencia.containsKey(string)) frequencia.put(string,frequencia.get(string)+1);
+                else frequencia.put(string,1);
+            }
+            else if(nota >=8.0 && nota<9.0){
+                String string = "[8.0, 9.0)";
+                if(frequencia.containsKey(string)) frequencia.put(string,frequencia.get(string)+1);
+                else frequencia.put(string,1);
+            }
+            else if(nota >=9.0 && nota<=10.0){
+                String string = "[9.0, 10.0]";
+                if(frequencia.containsKey(string)) frequencia.put(string,frequencia.get(string)+1);
+                else frequencia.put(string,1);
+            }
+        }
+    }
+
+    public void result(){
+        System.out.print("Notas: ");
+        for(float nota : this.notas){
+            System.out.print(nota+", ");
+        }
+        System.out.println("");
+        System.out.printf(
+                "%nMédia: %.2f"+
+                "%nMaior Nota: %.2f"+
+                "%nMenor Nota: %.2f"
+        ,media,max,min);
+
+        System.out.println("");
+        System.out.println(this.frequencia);
     }
 
     public String getNome(){
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        float[] notas = {5.5f,8.6f,6.9f,4.5f,7.3f};
+        GradeBook registro = new GradeBook("TI",4);
+        registro.addNotas(10.0f,0);
+        registro.addNotas(5.0f,1);
+        registro.addNotas(5.5f,2);
+        registro.addNotas(2.0f,3);
+
+        registro.calcular();
+        registro.result();
     }
 }
